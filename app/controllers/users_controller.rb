a1fff1bea5cf6ac924c410329412061fbb4d9c2a
Stef Claus <stef.claus@gmail.com>
2019-04-05 17:39:41 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,30 @@ class UsersController < ApplicationController
       user.save
       redirect '/tweets'
     end
-	end
+  end
 
+  get '/login' do
+    if !logged_in?
+      erb :"users/login"
+    else !!logged_in?
+      redirect '/tweets'
+    end
+  end
 
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+
+	  if user && user.authenticate(params[:password])
+	    session[:user_id] = user.id
+	    redirect "/tweets"
+	  else
+	    redirect "/login"
+	  end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/login"
+  end
 
 end
