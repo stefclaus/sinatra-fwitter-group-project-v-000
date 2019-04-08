ff11d14cb20b86943a39280b1acf306208525239
Stef Claus <stef.claus@gmail.com>
2019-04-08 22:06:56 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,10 +17,14 @@ class TweetsController < ApplicationController
     end
   end
 
-  #get '/tweets/:id' do
-  #  @tweet = Tweet.find(params[:id])
-  #  erb :'tweets/show_tweets'
-  #end
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show_tweets'
+    else
+      redirect '/login'
+    end
+  end
 
   post '/tweets/new' do
     if logged_in? && !params[:content].empty?
@@ -28,6 +32,36 @@ class TweetsController < ApplicationController
       @user.tweets << Tweet.create(:content => params[:content])
       redirect "/tweets"
     end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      @tweet.update(params[:content])
+      redirect"/tweets/tweets"
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/tweets/:id' do
+    if logged_in? && @user==current_user
+      @tweet = Tweet.find(params[:id])
+      @tweet.delete
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+
   end
 
 
